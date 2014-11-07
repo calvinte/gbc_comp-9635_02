eea13de7708de782ea8c42e0b7d2e0fc1d652dd9
Serge Fomin <cjfomin@hotmail.com>
2014-11-06 19:11:38 -0500

    week 1 in class

---
+++ week_01/fomin-serge/myscript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+$('iframe').hover(function(){
+	$('iframe').animate({
+		width: '960px',
+		height: '700px',
+		});
+	},
+	function(){
+		$('iframe').animate({
+		width: '600px',
+		height: '400px',
+		});
+	});
+});
