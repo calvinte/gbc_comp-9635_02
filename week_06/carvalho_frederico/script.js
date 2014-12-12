0bc25722c43ab8e79d3e6ab77b8c7d812c28b90f
Frederico Carvalho <fredincontact@hotmail.com>
2014-12-11 20:21:59 -0500

    Week 06
    Google Map and Instagram

---
+++ week_06/carvalho_frederico/script.js
@@ -0,0 +1,55 @@
+//console.log('test');
+//console.log(jQuery);
+//console.log(google);
+
+$(document).ready(function(){
+	var mapElement = $('#google-map')[0];
+	var lat =43.7000;
+	var lng =-79.4000;
+	var url = 'https://api.instagram.com/v1/media/search'
+	+'?lat=' + lat
+	+'&lng=' + lng
+	+'&distance=500'
+	+'&client_id=585d00be2af34a26b0e1caa6995cf19f';
+
+var openInfoWindow;
+
+	var map = new google.maps.Map(mapElement, {
+		zoom: 15,
+		center: new google.maps.LatLng(lat, lng),
+	})
+
+
+	$.ajax({
+		type:'GET',
+		dataType:'jsonp',
+		url: url,
+		success: function(response) {
+			response.data.forEach(function(photo) {
+				var pLatLng, pMarker, pInfoWindow, pUrl;
+				pLatLng = new google.maps.LatLng(
+					photo.location.latitude,
+					photo.location.longitude
+				);
+				pMarker = new google.maps.Marker({
+					position:pLatLng,
+					map: map,
+				});
+				pUrl = photo.images.low_resolution.url;
+				pInfoWindow = new google.maps.InfoWindow({
+					content:'<img src="' +pUrl + '"/>',
+				});
+				google.maps.event.addListener(pMarker,'click',function() {
+					if (openInfoWindow) openInfoWindow.close();
+					openInfoWindow = pInfoWindow;
+					pInfoWindow.open(map, pMarker);					
+				});
+			});
+		},
+	});
+});
+
+
+
+
+
